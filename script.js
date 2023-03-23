b56bb34131992fb58983716347c25c220602f52d
Aayush2609 <aadeep2609@gmail.com>
2023-03-23 22:13:11 +0530

    script cleaned

--- script.js
+++ script.js
@@ -5,14 +5,3 @@ fetch('https://icanhazdadjoke.com/slack')
         const jokeElement = document.getElementById('jokeElement');
         jokeElement.innerHTML = jokeText;
     })
-
-
-// fetch('https://newsapi.org/v2/top-headlines?country=in&apiKey=218c5f76a51049889049227a48a510e7')
-//     .then(data => data.json())
-//     .then(newsData => {
-//         window.onload = function what() {
-//             const newsText = newsData.articles[0].content;
-//             const newsElement = document.getElementById('newsElement');
-//             newsElement.innerHTML = newsText;
-//         }
-//     })
